a448994f0693209b87f2597d259781fdb68703fe
Yordan Halachev <yordan.i.halachev@gmail.com>
2020-09-08 13:36:44 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 terraform {
-    backend "remote" {
-         # The name of your Terraform Cloud organization.
+    backend "remote" {         
+# The name of your Terraform Cloud organization.
         organization = "yordanh_free"
 
          # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "2"
+        workspaces {
+            name = "2"
          }
-       }
-     }
+    }
+}
 
      # An example resource that does nothing.
-     resource "null_resource" "example" {
-       triggers = {
-         value = "A example resource that does nothing!"
-       }
-     }
+resource "null_resource" "example" {
+    triggers = {
+        value = "A example resource that does nothing!"
+    }
+}
