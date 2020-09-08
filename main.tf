fbf224f5be07248cc3b008500063269e2413aa25
YordanIH <yordan@hashicorp.com>
2020-09-08 13:39:16 +0300

    proper formatting

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 terraform {
-    backend "remote" {         
-# The name of your Terraform Cloud organization.
-        organization = "yordanh_free"
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "yordanh_free"
 
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-        workspaces {
-            name = "2"
-         }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "2"
     }
+  }
 }
 
-     # An example resource that does nothing.
+# An example resource that does nothing.
 resource "null_resource" "example" {
-    triggers = {
-        value = "A example resource that does nothing!"
-    }
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
 }
